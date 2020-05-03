213dfd540b7b5f890d99a6fecc0f5169e547310e
Konstrictor <jonkur96@gmail.com>
2020-05-03 15:55:17 +0200

    settings.py

--- RihaApp/settings.py
+++ RihaApp/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 LOGIN_URL = "/login/"
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

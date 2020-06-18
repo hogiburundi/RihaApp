93a0c3d23076e80bb07cee1f34a397ff571c887a
Konstrictor <jonkur96@gmail.com>
2020-06-18 15:27:33 +0200

    static correction for server version

--- RihaApp/urls.py
+++ RihaApp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.base.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 for app in os.listdir(APPS_DIR):

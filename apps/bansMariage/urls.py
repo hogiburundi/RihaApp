3a15fff19309a1060cb1e08328543da7deb2d930
ArtcalO97 <niyca14@gmail.com>
2020-06-30 23:50:31 +0200

    Required files for Attestation de Bans de Mariage are Staged

---
+++ apps/bansMariage/urls.py
@@ -0,0 +1,14 @@
+import os
+from django.urls import path
+from . import views
+
+BASE_NAME = os.path.split(os.path.split(os.path.abspath(__file__))[0])[1]
+
+urlpatterns = [
+    path('', views.DocumentListView.as_view(), name=BASE_NAME+"_list"),
+    path('form/', views.DocumentFormView.as_view(), name=BASE_NAME+"_form"),
+    path('form/addincognito/', views.Incognito, name=BASE_NAME+"_incognito"),
+    path('payform/<id_compl>', views.DocumentPayView.as_view(), name=BASE_NAME+"_payform"),
+    path('secretary/', views.SecretaryListView.as_view(), name=BASE_NAME+'_secr_list'),
+    path('secretary/<document_id>', views.SecretaryView.as_view(), name=BASE_NAME+'_secr_edit'),
+]

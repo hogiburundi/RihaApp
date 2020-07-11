908d9bae23ffd4724a580f67e7055e362efcf186
ArtcalO97 <niyca14@gmail.com>
2020-07-11 23:30:49 +0200

    Poss_Parcelle : All issues are fixesd such as :
    Temoin : unkown user error can not submit
    Preview d'un autre document (Etat civil)
    Pourquoi compléter le chef de quartier
    Section chef de quartier, apparaisse aussi le texte "chef de quartier taba"
    date et lieu de délivrance du CNI ne sont pas correct
    Le nom du chef de zone n'apparaisse pas
    A droite du bouton preview, Enlever le texte witness
    Pourquoi compléter le chef de quartier?

--- apps/atPossParcelle/models.py
+++ apps/atPossParcelle/models.py
@@ -6,11 +6,9 @@ from apps.base.models import *
 class Document(models.Model):
 	user = models.ForeignKey(User, related_name="atposs_parc_user", null=True, on_delete=models.SET_NULL)
 	zone = models.ForeignKey(Zone, related_name="atposs_parc_zone", max_length=64, null=True, on_delete=models.SET_NULL)
-	residence_quarter = models.ForeignKey(Quarter, related_name="atposs_parc_residence", max_length=64, null=True, on_delete=models.SET_NULL)
-	date = models.DateField(default=timezone.now)
-	first_witness = models.ForeignKey(User, related_name='atposs_parc_witness1',max_length=64, on_delete=models.CASCADE)
-	second_witness = models.ForeignKey(User, related_name='atposs_parc_witness2',max_length=64, on_delete=models.CASCADE)
-	quarter_leader = models.ForeignKey(User, related_name='atposs_parc_quater_leader',max_length=64, on_delete=models.CASCADE)
+	residence_quarter = models.ForeignKey(Quarter, related_name="atposs_residence_user", max_length=64, null=True, on_delete=models.SET_NULL)
+	quarter_propriety = models.ForeignKey(Quarter, related_name="atposs_parc_residence", max_length=64, null=True, on_delete=models.SET_NULL)
+	quarter_surface = models.FloatField(null=True, blank=True)
 	rejection_msg = models.TextField(null=True, blank=True)
 	secretary_validated = models.BooleanField(null=True)
 	ready = models.BooleanField(default=False)
@@ -24,7 +22,20 @@ class Document(models.Model):
 			return PriceHistory.objects.filter(zone=self.zone).last().total()
 		except:
 			return 0
+			
+	def save(self, *args, **kwargs):
+		super(Document, self).save(*args, **kwargs)
+		if self.ready:
+			Notification(self.user, f"l'identité complete que vous avez demandé le {self.date} à {self.zone} est disponible").save()
 
+	def payment_percent(self):
+		return 100 if self.zone_payment else 0
+
+	def validation_percent(self):
+		return 100 if self.secretary_validated  else 0
+
+	def __str__(self):
+		return f"{self.user} {self.zone}"
 
 class PriceHistory(models.Model):
 	date = models.DateField()
